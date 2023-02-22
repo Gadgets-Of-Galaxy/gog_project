9a3e73fe17c374d9dce4bc9f308adc2bd5c8b3e5
HemanthA0921 <97177861+HemanthA0921@users.noreply.github.com>
2023-02-22 14:09:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+require('./utils/db.config')
+const MongoStore = require('connect-mongo')(session)
+const mongoDbConnection = require('./utils/db.config')
+const passport = require('passport')
+require('./utils/authStrategies/localStrategy')
+const authMiddleware = require('./middlewares/authMiddlewares')
+const authRoutes = require('./routes/authRoutes')
+const app = express()
+const config = require('./utils/config')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+// app.set('trust proxy', 1)
+app.use(session({
+  secret: '788a154e2a8d07c4cafdee4a7d6dff2d90ab2586',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  store: new MongoStore({ mongooseConnection: mongoDbConnection })
+}))
+app.use(express.static('public'))
+app.use(logger('dev'))
+app.use(passport.initialize())
+app.use(passport.session())
+app.locals.message = {}
+app.locals.formData = {}
+app.locals.errors = {}
+
+app.use('/', authRoutes)
+
+app.get('/', (req, res) => {
+  // console.log('User:', req.user)
+  return res.render('index')
+})
+
+app.get('/homepage', authMiddleware, (req, res) => {
+  return res.render('dashboard')
+})
+
+app.use((req, res, next) => {
+  res.status(404).render('404')
+})
+
+app.listen(config.port, () => {
+  console.log(`Server running at port ${config.port}`)
+})
+
+module.exports = app
