b07be14befec384593b06b792955121e5d6a6f15
HemanthA0921 <97177861+HemanthA0921@users.noreply.github.com>
2023-02-23 13:04:21 +0530

    Create authRoutes.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,134 @@
+const express = require('express')
+const router = express.Router()
+const { addUser } = require('../modules/users/service/userService')
+const { registerSchema } = require('../modules/users/validations/authValidation')
+const { joiErrorFormatter, mongooseErrorFormatter } = require('../utils/validationFormatter')
+const passport = require('passport')
+const guestMiddleware = require('../middlewares/guestMiddleware')
+// const authMiddleware = require('../middlewares/authMiddlewares')
+const flasherMiddleware = require('../middlewares/flasherMiddleware')
+
+/**
+ * Shows page for user registration
+ */
+router.get('/register', guestMiddleware, flasherMiddleware, (req, res) => {
+  return res.render('register')
+})
+
+/**
+ * Handles user registration
+ */
+router.post('/register', guestMiddleware, async (req, res) => {
+  try {
+    const validationResult = registerSchema.validate(req.body, {
+      abortEarly: false
+    })
+    if (validationResult.error) {
+      req.session.flashData = {
+        message: {
+          type: 'error',
+          body: 'Validation Errors'
+        },
+        errors: joiErrorFormatter(validationResult.error),
+        formData: req.body
+      }
+      return res.redirect('/register')
+    }
+    await addUser(req.body)
+    req.session.flashData = {
+      message: {
+        type: 'success',
+        body: 'Registration success'
+      }
+    }
+    return res.redirect('/register')
+  } catch (e) {
+    req.session.flashData = {
+      message: {
+        type: 'error',
+        body: 'Validation Errors'
+      },
+      errors: mongooseErrorFormatter(e),
+      formData: req.body
+    }
+    return res.redirect('/register')
+  }
+})
+
+/**
+ * Shows page for user login
+ */
+router.get('/login', guestMiddleware, flasherMiddleware, (req, res) => {
+  return res.render('login')
+})
+
+/**
+ * Logs in a user
+ */
+router.post('/login', guestMiddleware, (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    console.log(err, user, info)
+    if (err) {
+      console.error('Err:', err)
+      req.session.flashData = {
+        message: {
+          type: 'error',
+          body: 'Login failed'
+        }
+      }
+      return res.redirect('/login')
+    }
+
+    if (!user) {
+      req.session.flashData = {
+        message: {
+          type: 'error',
+          body: info.message
+        }
+      }
+      return res.redirect('/login')
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        console.error('Err:', err)
+        req.session.flashData = {
+          message: {
+            type: 'error',
+            body: 'Login failed'
+          }
+        }
+      }
+      return res.redirect('/homepage')
+    })
+  })(req, res, next)
+})
+
+/**
+ * Logs out a user
+ */
+// router.get('/logout', authMiddleware, (req, res) => {
+//   req.logout()
+//   req.session.flashData = {
+//     message: {
+//       type: 'success',
+//       body: 'Logout success'
+//     }
+//   }
+//   return res.redirect('/')
+// })
+
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err) }
+    req.session.flashData = {
+      message: {
+        type: 'success',
+        body: 'Logout success'
+      }
+    }
+    res.redirect('/')
+  })
+})
+
+module.exports = router
